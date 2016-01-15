f62f8ca129abc3ae81b0a2cf65e282b765eec02a
Ioannis Koutras <ioannis.koutras@gmail.com>
2016-01-15 13:32:39 +0200

    Expose force SSL option in Tracks

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,11 @@ cd /usr/local/share/tracks
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake assets:precompile RAILS_ENV=production
 
-for variable in TRACKS_SALT TRACKS_SECRET_TOKEN TRACKS_ADMIN_EMAIL; do
-  sed -ri "s/[{]{2}$variable[}]{2}/\${$variable}/g" config/site.yml
+TRACKS_FORCE_SSL=${TRACKS_FORCE_SSL:-false}
+
+for variable in TRACKS_SALT TRACKS_SECRET_TOKEN TRACKS_ADMIN_EMAIL \
+  TRACKS_FORCE_SSL; do
+    sed -ri "s/[{]{2}$variable[}]{2}/\${$variable}/g" config/site.yml
 done
 
 exec /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
