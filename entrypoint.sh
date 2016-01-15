a35f25f7bc65f08ae5c9a01f76c7c80fbf5b66df
Ioannis Koutras <ioannis.koutras@gmail.com>
2016-01-15 13:45:42 +0200

    Use directly environment variables in Rails configuration file

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,6 @@ bundle exec rake assets:precompile RAILS_ENV=production
 
 TRACKS_FORCE_SSL=${TRACKS_FORCE_SSL:-false}
 
-for variable in TRACKS_SALT TRACKS_SECRET_TOKEN TRACKS_ADMIN_EMAIL \
-  TRACKS_FORCE_SSL; do
-    sed -ri "s/[{]{2}$variable[}]{2}/\${$variable}/g" config/site.yml
-done
-
 exec /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
 
 exit 0
