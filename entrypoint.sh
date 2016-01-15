4a9978cc3d3da4b759ca3df5df06ff5c6647a540
Ioannis Koutras <ioannis.koutras@gmail.com>
2016-01-15 13:58:38 +0200

    Generate secret token if none given

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ cd /usr/local/share/tracks
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake assets:precompile RAILS_ENV=production
 
+TRACKS_SECRET_TOKEN=${TRACKS_SECRET_TOKEN:-$(bundle exec rake secret)}
 TRACKS_FORCE_SSL=${TRACKS_FORCE_SSL:-false}
 
 exec /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
